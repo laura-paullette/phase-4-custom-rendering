154fa2b77f4613dae1f3d62b79acc9521621b5ed
laura-paullette <laura.paullette@student.moringaschool.com>
2023-06-20 13:34:30 +0300

    completed

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -8,8 +8,21 @@ class CheesesController < ApplicationController
 
   # GET /cheeses/:id
   def show
-    cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    # cheese = Cheese.find_by(id: params[:id])
+    # render json: cheese
+
+  #   cheese = Cheese.find_by(id: params[:id])
+  # render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+
+  cheese = Cheese.find_by(id: params[:id])
+  if cheese
+    render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+  else
+    render json: { error: 'Cheese not found' }
   end
+  
+  end
+
+
 
 end
